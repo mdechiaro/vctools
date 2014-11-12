4b2b42c2d46129746906536e6d5f0e34b3cb4050
Mark Dechiaro <mdechiaro@uchicago.edu>
2014-11-12 10:34:42 -0600

    renaming console methods, and adding method to generate a thumbprint from the session ticket

--- vctools/console.py
+++ vctools/console.py
@@ -1,9 +1,38 @@
 #!/usr/bin/python
+import re
+
 
 class Console(object):
     def __init__(self):
         pass
-    def generate_url(self, vc, vmid, name, host, ticket):
-        return 'http://%s:7331/console/?vmId=%s&vmName=%s&host=%s&sessionTicket=%s' % (
-            vc, vmid, name, host, ticket)
+
+
+    def mkthumbprint(self, ticket):
+        """
+        This method uses the information in a ticket to generate a thumbprint.
+        The string is split on --, and uses the second part to convert to the
+        appropriate format.
+
+        :param ticket: vim.SessionManager.AcquireCloneTicket()
+        """
+        # cst is a placeholder
+        cst, tp = ticket.split('--')
+        thumbprint = re.sub(r'tp-', '', tp)
+        thumbprint = re.sub(r'-', ':', thumbprint)
+        return thumbprint
+
+
+    def mkurl(self, vmid, name, vc, ticket, thumbprint):
+        """
+        We have a Tomcat broker (web) which has a different hostname than our 
+        vCenter, so we need to account for that.  The hostname appends -web to
+        vc hostname.
+        """
+
+        host, domain, tld  = vc.split('.')
+        host = host + '-web'
+        web = '.'.join((host, domain, tld))
+
+        return 'http://%s:7331/console/?vmId=%s&vmName=%s&host=%s&sessionTicket=%s&thumbprint=%s' % (
+            web, vmid, name, vc, ticket, thumbprint)
 
