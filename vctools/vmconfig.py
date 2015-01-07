9c45400a115b5140e9b689fb355d860bf90a43c7
Mark Dechiaro <mdechiaro@uchicago.edu>
2015-01-07 11:05:27 -0600

    cdrom needs key value to edit device to mount an iso

--- vctools/vmconfig.py
+++ vctools/vmconfig.py
@@ -170,6 +170,11 @@ class VMConfig(Query):
             cdrom.device = vim.vm.device.VirtualCdrom()
             # controllerKey is tied to IDE Controller
             cdrom.device.controllerKey = 201
+            cdrom.device.unitNumber = 0
+            # key is needed to mount the iso, need to verify if this value
+            # changes per host, and if so, then logic needs to be added to
+            # obtain it
+            cdrom.device.key = 3002
 
             cdrom.device.backing = vim.vm.device.VirtualCdrom.IsoBackingInfo()
             cdrom.device.backing.fileName = '['+ datastore + '] ' + iso_path
