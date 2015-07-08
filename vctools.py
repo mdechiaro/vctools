28e91b33355740644c3a142de003052efe680549
Mark Dechiaro <mdechiaro@uchicago.edu>
2015-07-08 14:01:42 -0500

    Updating version number

--- vctools.py
+++ vctools.py
@@ -26,7 +26,7 @@ class VCTools(object):
     """
     # pylint: disable=too-many-instance-attributes
     def __init__(self):
-        self.__version__ = '1.1'
+        self.__version__ = '1.2'
         self.auth = None
         self.clusters = None
         self.datacenters = None
