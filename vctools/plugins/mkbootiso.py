2916b281fe4a7e46dfe2eab0830078589ee60bf4
Mark Dechiaro <mdechiaro@users.noreply.github.com>
2015-11-12 13:51:08 -0600

    Adding new mkbootiso plugin.
    This plugin will allow a per host kickstart boot iso to be created upon
    VM creation.

---
+++ vctools/plugins/mkbootiso.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+"""Plugin for creating a boot iso on a per server basis."""
+import subprocess
+import sys
+import textwrap
+
+class MkBootISO(object):
+    """
+    Makes a static IP per host boot iso for easier network host creation when
+    PXE booting or DHCP networks are not an option.
+    """
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def updateiso(cls, source, **kwargs):
+        """
+        Update iso image with host specific parameters.
+        All kickstart options will be added from a yaml file.
+        """
+
+        label = """
+            default vesamenu.c32
+            display boot.msg
+            timeout 5
+            label iso created by %s
+            menu default
+            kernel vmlinuz
+            append initrd=initrd.img %s %s
+
+            """ % (sys.argv[0],
+                ' '.join("%s=%s" % (key, val)
+                    for (key, val) in kwargs['kickstart'].iteritems()),
+                ' '.join("%s=%s" % (key, val)
+                    for (key, val) in kwargs['options'].iteritems())
+            )
+
+        with open(source + '/isolinux/isolinux.cfg', 'w') as iso_cfg:
+            iso_cfg.write(textwrap.dedent(label).strip())
+
+
+    @classmethod
+    def createiso(cls, source, output, filename):
+        """create iso image."""
+        cmd = ['/usr/bin/genisoimage',
+               '-J',
+               '-T',
+               '-o', output + filename,
+               '-b', 'isolinux/isolinux.bin',
+               '-c', 'isolinux/boot.cat',
+               '-no-emul-boot',
+               '-boot-load-size', '4',
+               '-boot-info-table', '-R',
+               '-m', 'TRANS.TBL',
+               '-graft-points', source,
+        ]
+
+        subprocess.call(cmd)
