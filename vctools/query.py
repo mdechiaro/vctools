8afc69a3d206accb17c51b782a79fb6fa2eaad25
Mark Dechiaro <mdechiaro@users.noreply.github.com>
2015-11-13 09:49:50 -0600

    query: Converting methods to class methods for easier access.

--- vctools/query.py
+++ vctools/query.py
@@ -10,7 +10,7 @@ class Query(object):
     and networks.
     """
     def __init__(self):
-        self.datastore_info = None
+        pass
 
 
     @classmethod
@@ -77,7 +77,8 @@ class Query(object):
             return [getattr(obj, attr) for obj in container]
 
 
-    def folders_lookup(self, container, datacenter, name):
+    @classmethod
+    def folders_lookup(cls, container, datacenter, name):
         """
         Returns the object for a folder name.  Currently, it only searches for
         the folder through one level of subfolders. This method is needed for
@@ -89,7 +90,7 @@ class Query(object):
             name (str):       Name of folder
         """
 
-        obj = self.get_obj(container, datacenter)
+        obj = Query.get_obj(container, datacenter)
 
         if hasattr(obj, 'vmFolder'):
             for folder in obj.vmFolder.childEntity:
@@ -102,7 +103,8 @@ class Query(object):
                             if item.name == name:
                                 return item
 
-    def list_vm_folders(self, container, datacenter):
+    @classmethod
+    def list_vm_folders(cls, container, datacenter):
         """
         Returns a list of Virtual Machine folders.  Sub folders will be listed
         with its parent -> subfolder. Currently it only searches for one
@@ -112,7 +114,7 @@ class Query(object):
             container (obj):  Container object
             datacenter (str): Name of datacenter
         """
-        obj = self.get_obj(container, datacenter)
+        obj = Query.get_obj(container, datacenter)
         folders = []
 
         if hasattr(obj, 'vmFolder'):
@@ -126,7 +128,8 @@ class Query(object):
         return folders
 
 
-    def datastore_most_space(self, container, cluster):
+    @classmethod
+    def datastore_most_space(cls, container, cluster):
         """
         Attempts to find the datastore with the most free space.
 
@@ -135,7 +138,7 @@ class Query(object):
             cluster (str):   Name of cluster
 
         """
-        obj = self.get_obj(container, cluster)
+        obj = Query.get_obj(container, cluster)
         datastores = {}
         for datastore in obj.datastore:
             # if datastore is a VMware File System
@@ -151,7 +154,8 @@ class Query(object):
 
 
     # pylint: disable=too-many-locals
-    def return_datastores(self, container, cluster, header=True):
+    @classmethod
+    def return_datastores(cls, container, cluster, header=True):
         """
         Returns a summary of disk space for datastores listed inside a
         cluster. Identical to list_datastore_info, but returns the datastores
@@ -163,15 +167,15 @@ class Query(object):
             header (bool):   Enables a header of info to datastore list.
         """
 
-        obj = self.get_obj(container, cluster)
+        obj = Query.get_obj(container, cluster)
 
-        self.datastore_info = []
+        datastore_info = []
 
         if header:
             header = [
             'Datastore', 'Capacity', 'Provisioned', 'Pct', 'Free Space', 'Pct'
             ]
-            self.datastore_info.append(header)
+            datastore_info.append(header)
 
         for datastore in obj.datastore:
             info = []
@@ -191,28 +195,29 @@ class Query(object):
             free_pct = '{0:.2%}'.format((free / capacity))
 
             info.append(datastore.name)
-            info.append(self.disk_size_format(capacity))
-            info.append(self.disk_size_format(provisioned))
+            info.append(Query.disk_size_format(capacity))
+            info.append(Query.disk_size_format(provisioned))
             info.append(provisioned_pct)
-            info.append(self.disk_size_format(free))
+            info.append(Query.disk_size_format(free))
             info.append(free_pct)
 
-            self.datastore_info.append(info)
+            datastore_info.append(info)
 
 
         # sort by datastore name
-        self.datastore_info.sort(key=lambda x: x[0])
+        datastore_info.sort(key=lambda x: x[0])
 
-        return self.datastore_info
+        return datastore_info
 
 
-    def list_vm_info(self, container, datacenter):
+    @classmethod
+    def list_vm_info(cls, container, datacenter):
         """
         Returns a list of names for VMs located inside a datacenter.
 
         """
 
-        obj = self.get_obj(container, datacenter)
+        obj = Query.get_obj(container, datacenter)
 
         vms = {}
 
@@ -229,13 +234,14 @@ class Query(object):
         return vms
 
 
-    def get_vmid_by_name(self, container, datacenter, name):
+    @classmethod
+    def get_vmid_by_name(cls, container, datacenter, name):
         """
         Returns the moId of matched name
 
         """
 
-        obj = self.get_obj(container, datacenter)
+        obj = Query.get_obj(container, datacenter)
 
         # recurse through datacenter object attributes looking for vm that
         # matches hostname.
