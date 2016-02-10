26826229aa2b4c7dff4f51ea6e830fcc4037dedd
Mark Dechiaro <mdechiaro@users.noreply.github.com>
2016-02-10 09:36:49 -0600

    argparser: updating version to 0.1.4

--- vctools/argparser.py
+++ vctools/argparser.py
@@ -9,7 +9,7 @@ import yaml
 class ArgParser(object):
     """Placeholder."""
     def __init__(self):
-        self.__version__ = '0.1.3'
+        self.__version__ = '0.1.4'
         self.help = None
         self.opts = None
 
