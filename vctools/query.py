292740a212c8299c580083c480e0f3188da48e9c
Mark Dechiaro <mdechiaro@users.noreply.github.com>
2016-02-16 22:54:42 -0600

    query: pylint removal

--- vctools/query.py
+++ vctools/query.py
@@ -153,7 +153,6 @@ class Query(object):
                 print(key)
 
 
-    # pylint: disable=too-many-locals
     @classmethod
     def return_datastores(cls, container, cluster, header=True):
         """
