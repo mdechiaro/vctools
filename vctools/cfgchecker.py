41ba653166ea63bf7d2fb21e9434bd7d32c812dc
Mark Dechiaro <mdechiaro@users.noreply.github.com>
2018-12-04 15:25:29 -0600

    Fix prompt descriptions on selections

--- vctools/cfgchecker.py
+++ vctools/cfgchecker.py
@@ -36,7 +36,7 @@ class CfgCheck:
                 guestid = cfg['vmconfig']['guestId']
             else:
                 guestid = Prompts.guestids()
-                print('\n%s selected.' % (guestid))
+                print('\n%s guestid selected.' % (guestid))
             # cluster
             if 'cluster' in cfg['vmconfig']:
                 cluster = cfg['vmconfig']['cluster']
@@ -44,17 +44,17 @@ class CfgCheck:
             else:
                 cluster = Prompts.clusters(auth.session)
                 cluster_obj = Query.get_obj(clusters.view, cluster)
-                print('\n%s selected.' % (cluster))
+                print('\n%s cluster selected.' % (cluster))
             # datastore
             if 'datastore' in cfg['vmconfig']:
                 datastore = cfg['vmconfig']['datastore']
             else:
                 datastore = Prompts.datastores(auth.session, cluster)
-                print('\n%s selected.' % (datastore))
+                print('\n%s datastore selected.' % (datastore))
             # datacenter
             if not opts.datacenter:
                 datacenter = Prompts.datacenters(auth.session)
-                print('\n%s selected.' % (datacenter))
+                print('\n%s datacenter selected.' % (datacenter))
             else:
                 datacenter = opts.datacenter
             # nics
@@ -63,13 +63,13 @@ class CfgCheck:
                 print('nics: %s' % (nics))
             else:
                 nics = Prompts.networks(cluster_obj)
-                print('\n%s selected.' % (','.join(nics)))
+                print('\n%s networks selected.' % (','.join(nics)))
             # folder
             if 'folder' in cfg['vmconfig']:
                 folder = cfg['vmconfig']['folder']
             else:
                 folder = Prompts.folders(auth.session, datacenter)
-                print('\n%s selected.' % (folder))
+                print('\n%s folder selected.' % (folder))
         else:
             name = Prompts.name()
             guestid = Prompts.guestids()
